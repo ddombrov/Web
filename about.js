a23d0cd7a40b8affd9d4e4c2b0634ebee16f9fb1
Daniel <ddddombrovsky@gmail.com>
2024-01-09 11:18:20 -0500

    testing

--- about.js
+++ about.js
@@ -1,23 +1,23 @@
-const getJokeBtn = document.getElementById("getJokeBtn"); // add id
-const jokeText = document.getElementById("jokeText"); // add id
+// const getJokeBtn = document.getElementById("getJokeBtn"); // add id
+// const jokeText = document.getElementById("jokeText"); // add id
 
-getJokeBtn.addEventListener("click", () => { // add event listener type
-    jokeText.textContent = "Fetching a joke...";
+// getJokeBtn.addEventListener("click", () => { // add event listener type
+//     jokeText.textContent = "Fetching a joke...";
 
-    fetch("https://api.api-ninjas.com/v1/jokes?limit=1", { // add event request url
-        headers: { 'X-Api-Key': '6VY8ptrsVvHVHmDwgZIk2g==MJRkl1yFv6SgLrx9' } // add api key
-    })
-        .then((response) => {
-            if (!response.ok) throw new Error("Network response was not ok");
-            return response.json();
-        })
-        .then((data) => {
+//     fetch("https://api.api-ninjas.com/v1/jokes?limit=1", { // add event request url
+//         headers: { 'X-Api-Key': '6VY8ptrsVvHVHmDwgZIk2g==MJRkl1yFv6SgLrx9' } // add api key
+//     })
+//         .then((response) => {
+//             if (!response.ok) throw new Error("Network response was not ok");
+//             return response.json();
+//         })
+//         .then((data) => {
 
-            jokeText.textContent = data[0].joke;
+//             jokeText.textContent = data[0].joke;
 
-        })
-        .catch((error) => {
-            console.error("Error fetching joke:", error);
-            jokeText.textContent = "An error occurred while fetching the joke.";
-        });
-});
+//         })
+//         .catch((error) => {
+//             console.error("Error fetching joke:", error);
+//             jokeText.textContent = "An error occurred while fetching the joke.";
+//         });
+// });
